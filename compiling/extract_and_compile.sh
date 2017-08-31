201c776576d469b782407d7a414bc19534ec14e3
tszhonglau <lauxx265@morris.umn.edu>
2017-08-31 00:29:45 -0500

    All test passed, modified extract and compile.sh

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,9 +1,8 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-tar -xvzf NthPrime.tgz -C $2
+tar -xf NthPrime.tgz -C $2 #Extract tar file and then place the extracted file into the command line argument directory
+cd $2/NthPrime #Open the NthPrime directory
 
-cd $2/NthPrime
+gcc -o NthPrime main.c nth_prime.c #Execute main.c and nth_prime.c in NthPrime
+./NthPrime $1 #Provide the command line argument for NthPrime
 
-gcc -o NthPrime main.c nth_prime.c
-
-./NthPrime $1
